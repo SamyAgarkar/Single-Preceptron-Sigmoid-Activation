bfb638721eaa370f4820990ba6c59bc9a997ab8a
SamyAgarkar <samyak.agarkar@gmail.com>
2017-02-16 19:15:41 +0530

    Add files via upload

---
+++ ANN.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ann;
+
+/**
+ *
+ * @author samy
+ */
+
+class Node{
+    float Weight;
+    
+    Node(){}
+    Node(float w){
+        Weight = w;
+        
+    }
+}
+class NeuralNet{
+    Node[] Nodes;
+    int InputSize;
+    float LearningRate;
+    
+    
+    NeuralNet(){}
+    
+    NeuralNet(int Size,float weights[],float LR){
+        InputSize=Size;
+        LearningRate=LR;
+        
+        Nodes = new Node[Size];
+        for(int i=0;i<Size;i++)
+            Nodes[i] = new Node(weights[i]);
+    }
+    
+    void fit(int input[], int output){
+        float functionInput =0.0f;                                               //input to Function f(a).Here a = sum of (inputs*weights)
+        
+        for(int i=0;i<InputSize;i++){
+            functionInput += Nodes[i].Weight * (float)input[i];
+        }
+        float out = 1/(1 + (float)Math.exp(-functionInput));
+        
+        for(int i=0;i<InputSize;i++){
+            Nodes[i].Weight +=  input[i]*(output-out)*out*(1-out);
+            //System.out.println(out+"  "+Nodes[0].Weight+"  "+Nodes[1].Weight+"  "+Nodes[2].Weight);
+        }
+        
+    }
+    
+    float predict(int input[]){
+        float functionInput=0.0f; 
+        
+        for(int i=0;i<InputSize;i++){
+            functionInput += Nodes[i].Weight * (float)input[i];
+        }
+        return 1/(1 + (float)Math.exp(-functionInput));
+    }
+}
+
+public class ANN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("****Artificial Neural Network (Single Node)****");
+        int[][] train_input = {{0,1,1},{1,0,0},{1,1,1},{0,1,1}};
+        int[] train_output = {0 , 1 , 1 , 0};
+        int size = 3;
+        int Repeatations = 100000;
+        float[] weights = {0f , 0f , 0f};
+        NeuralNet NN = new NeuralNet(size,weights,0.0001f);
+        
+        for(int j=0;j<Repeatations;j++){
+            for(int i=0;i<size;i++){
+                NN.fit(train_input[i], train_output[i]);
+            }
+        }
+        
+        int[] test_input = {1 , 0 , 1}; 
+        System.out.println("Predicted Output is " + NN.predict(test_input));
+        
+    }
+    
+}
